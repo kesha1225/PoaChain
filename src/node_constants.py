90738994db16f0f6b1ba8f44db51d5f48f6a61c9
kesha1225 <samedov03@mail.ru>
2024-06-07 15:03:24 +0300

    new text

--- src/node_constants.py
+++ src/node_constants.py
@@ -14,17 +14,17 @@ def sort_nodes(nodes: list[NodeConstant]) -> list[NodeConstant]:
 
 
 YANDEX_NODE = NodeConstant(
-    title_id="Yandex-1",
+    title_id="Tindex-1",
     url="http://127.0.0.1:1234",
     public_key="3076124a6d30a1125e916bbbc99a8470f25ced4c777c1fd9c14d68fb275a3641",
 )
 SBER_NODE = NodeConstant(
-    title_id="Sber-1",
+    title_id="Sver-1",
     url="http://127.0.0.1:3456",
     public_key="b0b9d00d6a948e70c05173f2888150ae3945ae619526b85e0bba796ba68c5f92",
 )
 GAZ_NODE = NodeConstant(
-    title_id="GAZ-1",
+    title_id="GBZ-1",
     url="http://127.0.0.1:7890",
     public_key="b49a1e105f4e8921d980d6f65791a7d843724b92f69d92065243e917c19427f4",
 )
