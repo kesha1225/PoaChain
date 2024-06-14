3ea96f8606f0add9def9fc9ca30846c9d1e58389
kesha1225 <samedov03@mail.ru>
2024-06-14 13:58:00 +0300

    fix cache

--- src/node/cache.py
+++ src/node/cache.py
@@ -1,15 +1,17 @@
+import typing
+
 import aiohttp
 
 
-def key_builder_is_alive(url: str, session: aiohttp.ClientSession) -> str | None:
+def key_builder_is_alive(function: typing.Callable, url: str, session: aiohttp.ClientSession) -> str | None:
     return f"is_node_online_{url}"
 
 
-def key_builder_is_ready(url: str, session: aiohttp.ClientSession) -> str | None:
+def key_builder_is_ready(function: typing.Callable, url: str, session: aiohttp.ClientSession) -> str | None:
     return f"is_node_ready_{url}"
 
 
 def key_builder_get_transactions_from_node(
-    url: str, session: aiohttp.ClientSession, address: str, transaction_type: str
+    function: typing.Callable, url: str, session: aiohttp.ClientSession, address: str, transaction_type: str
 ) -> str | None:
     return f"get_transactions_from_node_{url}_{address}_{transaction_type}"
